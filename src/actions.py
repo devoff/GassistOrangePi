2dbe45d043f5024eda01cca32f998d14db7d03da
devoff <4802297@gmail.com>
2018-03-28 17:02:51 +0300

    test
    rrr

---
+++ src/actions.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+#This is different from AIY Kit's actions
+#Copying and Pasting AIY Kit's actions commands will not work
+
+import os
+import os.path
+import RPi.GPIO as GPIO
+import time
+import re
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#Number of entities in 'var' and 'PINS' should be the same
+var = ('kitchen lights', 'bathroom lights', 'bedroom lights')#Add whatever names you want. This is case is insensitive
+gpio = (23,24,25)#GPIOS for 'var'. Add other GPIOs that you want
+
+#Number of station names and station links should be the same
+stnname=('Radio One', 'Radio 2', 'Radio 3', 'Radio 5')#Add more stations if you want
+stnlink=('http://www.radiofeeds.co.uk/bbcradio2.pls', 'http://www.radiofeeds.co.uk/bbc6music.pls', 'http://c5icy.prod.playlists.ihrhls.com/1469_icy', 'http://playerservices.streamtheworld.com/api/livestream-redirect/ARNCITY.mp3')
+
+#IP Address of ESP
+ip='xxxxxxxxxxxx'
+
+#Declaration of ESP names
+devname=('Device 1', 'Device 2', 'Device 3')
+devid=('/Device1', '/Device2', '/Device3')
+
+for pin in gpio:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+
+#Servo pin declaration
+GPIO.setup(27, GPIO.OUT)
+pwm=GPIO.PWM(27, 50)
+pwm.start(0)
+
+playshell = None
+
+def radio(phrase):
+    for num, name in enumerate(stnname):
+        if name.lower() in phrase:
+            station=stnlink[num]
+            p = subprocess.Popen(["/usr/bin/vlc",station],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+
+def ESP(phrase):
+    for num, name in enumerate(devname):
+        if name.lower() in phrase:
+            dev=devid[num]
+            if 'on' in phrase:
+                ctrl='=ON'
+            elif 'off' in phrase:
+                ctrl='=OFF'
+            subprocess.Popen(["elinks", ip + dev + ctrl],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+            time.sleep(2)
+            subprocess.Popen(["/usr/bin/pkill","elinks"],stdin=subprocess.PIPE)
+
+                    
+def SetAngle(angle):
+    duty = angle/18 + 2
+    GPIO.output(27, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    pwm.ChangeDutyCycle(0)
+    GPIO.output(27, False)
+
+def YouTube(phrase):
+    idx=phrase.find('play')
+    track=phrase[idx:]
+    track=track.replace("'}", "",1)
+    track = track.replace('play','',1)
+    track=track.strip()
+    global playshell
+    if (playshell == None):
+        playshell = subprocess.Popen(["/usr/local/bin/mpsyt",""],stdin=subprocess.PIPE ,stdout=subprocess.PIPE)
+    print("Playing: " + track)
+    playshell.stdin.write(bytes('/' + track + '\n1\n'))
+    playshell.stdin.flush()
+
+def stop():
+    pkill = subprocess.Popen(["/usr/bin/pkill","vlc"],stdin=subprocess.PIPE)
+    os.system("pkill chromium")
+    
+
+def Action(phrase):
+    if 'shut down' in phrase:
+        subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Pi-Close.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(10)
+        os.system("sudo shutdown -h now")
+        #subprocess.call(["shutdown -h now"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if 'servo' in phrase:
+        for s in re.findall(r'\b\d+\b', phrase):
+            SetAngle(int(s))
+    if 'zero' in phrase:
+        SetAngle(0)
+    else:
+        for num, name in enumerate(var):
+            if name.lower() in phrase:
+                pinout=gpio[num]
+                if 'on' in phrase:
+                    GPIO.output(pinout, 1)
+                    subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Device-On.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                elif 'off' in phrase:
+                    GPIO.output(pinout, 0)
+                    subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Device-Off.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
